bd11be6cfc33d67c64b05eb9bfb184dca8112bb2
decosawa <andreluiiz.gst@gmail.com>
2022-04-12 14:46:20 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+let app = express();
+
+app.get('/', (req, res)=>{
+
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end("<h1>Olá</h1>");
+
+});
+app.get('/users',(req,res) =>{
+
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'application/html');
+    res.json({
+
+        users:[{
+        name: 'deco',
+        email: 'deco@deco.com',
+        id:1
+        }]
+
+    });
+
+
+});
+
+app.listen(3000, "127.0.0.1", ()=>{
+
+    console.log("Servidor rodando!");
+
+})
